abe4ca30ef65d06e9488dcf053065c61c8138565
mattiapisilli <dev.mattiapisilli@gmail.com>
2021-05-12 10:32:28 +0200

    Aggiunta model e connessione al DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:nid_notes/screens/home.dart';
 import 'package:nid_notes/screens/login.dart';
 import 'package:nid_notes/screens/splash.dart';
 
-void main() {
+void main() async {
+  // Avoid errors caused by flutter upgrade.
+  // Importing 'package:flutter/widgets.dart' is required.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Open the database and store the reference.
+  final Future<Database> database = openDatabase(
+    // Set the path to the database. Note: Using the `join` function from the
+    // `path` package is best practice to ensure the path is correctly
+    // constructed for each platform.
+    join(await getDatabasesPath(), 'nid_notes.db'),
+  );
   runApp(NIDNotes());
 }
 
