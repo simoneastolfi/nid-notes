2281508b0853e6f923e7c8eb40c2636809db092a
eleonora proietti costa <eleonoraproietticosta@gmail.com>
2021-05-12 10:32:36 +0200

    ho aggiunto il model e la connessione al DB

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,20 @@ import 'package:nid_notes/screens/home.dart';
 import 'package:nid_notes/screens/login.dart';
 import 'package:nid_notes/screens/splash.dart';
 
-void main() {
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+// Open the database and store the reference.
+  final Future<Database> database = openDatabase(
+    // Set the path to the database. Note: Using the `join` function from the
+    // `path` package is best practice to ensure the path is correctly
+    // constructed for each platform.
+    join(await getDatabasesPath(), 'nid_notes.db'),
+  );
+
+
   runApp(NIDNotes());
 }
 
---
+++ lib/models/note.dart
@@ -0,0 +1,9 @@
+class Note{
+  final int id;
+  final String title;
+  final String content;
+
+  Note(this.id, this.title, this.content);
+}
+
+
